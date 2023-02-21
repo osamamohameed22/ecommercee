c6d38035cc240c58671bf501a2716f5894682a23
osama mohamed <osamamohamed@gmail.com>
2023-02-21 21:46:46 +0200

    carousel feature

--- src/index.js
+++ src/index.js
@@ -17,6 +17,64 @@ $(document).ready(function(){
         $(this).parents('.product-option').addClass('active');
     });
 
+    $('[data-product-quantity]').change(function(){
+        var newQuantity=$(this).val();
+        var parent=$(this).parents('[data-product-info]');
+        var pricePerUnit=parent.attr('data-product-price');
+        var totalPriceForProduct=newQuantity*pricePerUnit;
+        parent.find('.total-price-for-product').text(totalPriceForProduct+'$');
+        calculateTotalPrice();
+    });
+
+    $('[data-remove-from-cart]').click(function(){
+        $(this).parents('[data-product-info]').remove();
+        calculateTotalPrice();
+    });
+
+    function calculateTotalPrice(){
+        var totalPriceForAllProduct=0;
+        $('[data-product-info]').each(function(){
+            var pricePerUnit=$(this).attr('data-product-price');
+            var quantity=$(this).find('[data-product-quantity]').val();
+            var totalPriceForProduct=pricePerUnit*quantity;
+            totalPriceForAllProduct=totalPriceForAllProduct+totalPriceForProduct;
+        });
+        $('#total-price-for-all-products').text(totalPriceForAllProduct+'$')
+    }
+
+    var citiesByCountry={
+        sa:['جده','الرياض'],
+        eg:['القاهره','طنطا'],
+        jo:['الزرقاء','عمان'],
+        ea:['دبي','ابو ظبي'],
+    };
+    $('#form-checkout select[name="country"]').change(function(){
+        var country=$(this).val();
+        var cities=citiesByCountry[country];
+        $('#form-checkout select[name="city"]').empty();
+        $('#form-checkout select[name="city"]').append(
+            '<option disablwd selected value=""> اختر المدينه </option>'
+        );
+        cities.forEach(function(city){
+            var newOption =$('<option></option>');
+            newOption.text(city);
+            newOption.val(city);
+            $('#form-checkout select[name="city"]').append(newOption);
+        });
+    });
+    $('#form-checkout input[name="payment-method"]').change(function(){
+        var paymentMethod=$(this).val();
+        if(paymentMethod==='on-delivery'){
+            $('#credit-card-info input').prop('disabled',true);
+
+        }else{
+            $('#credit-card-info input').prop('disabled',false)
+        }
+        $('#credit-card-info').toggle();
+    });
+
+
+
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,6 @@ module.exports = {
         use: [
           {
             loader: "html-loader",
-            options: {
-              minimize: true,
-            },
           },
         ],
       },
@@ -88,6 +85,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "product.html",
       template: "./src/product.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "checkout.html",
+      template: "./src/checkout.html",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: "payment.html",
+      template: "./src/payment.html",
     }),
 
     new MiniCssExtractPlugin({
